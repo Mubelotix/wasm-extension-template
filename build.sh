0f8eb8fdb4ca4c81452500f8e45625bc83b4dd05
Mubelotix <mubelotix@gmail.com>
2022-11-03 11:00:08 +0100

    Add manifest v3 support

--- build.sh
+++ build.sh
@@ -2,5 +2,25 @@
 
 rm -rf pkg
 wasm-pack build --target=no-modules || exit 1
-cp manifest.json pkg/manifest.json
+
+# Find value of argument --manifest-version
+for arg in "$@"; do
+  case "$arg" in
+    --manifest-version=*)
+      manifest_version="${arg#*=}"
+      ;;
+  esac
+done
+
+# Copy manifest.json to pkg
+if [ "$manifest_version" = "v3" ] || [ "$manifest_version" = "3" ]; then
+  cp manifest_v3.json pkg/manifest.json
+else if [ "$manifest_version" = "v2" ] || [ "$manifest_version" = "2" ]; then
+  cp manifest_v2.json pkg/manifest.json
+else
+  echo "Packaging with manifest version v2. Pass --manifest-version=v3 to package with manifest version 3."
+  cp manifest_v2.json pkg/manifest.json
+  fi
+fi
+
 printf "async function run() {\n\tawait wasm_bindgen(chrome.extension.getURL('{{crate_name}}_bg.wasm'));\n}\n\nrun();" >> pkg/run_wasm.js
